b35fd7fdd9500677463b4081178d099dc20cae0b
Ruben <RubenHarmSmits@gmail.com>
2019-12-02 21:38:05 +0100

    first 2 puzzles2

---
+++ AoC.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public abstract class AoC {
+    ArrayList<String> input = new ArrayList<>();
+
+    ArrayList<String> parseFileEnter() throws IOException {
+        File fl = new File("Input");
+        BufferedReader br = new BufferedReader(new FileReader(fl));
+        String line;
+        while((line = br.readLine())!=null){
+            input.add(line.trim());
+        }
+
+        return input;
+    }
+
+    ArrayList<String> parseFileSplit() throws IOException {
+        File fl = new File("Input");
+        BufferedReader br = new BufferedReader(new FileReader(fl));
+        String line = br.readLine();
+
+        String[] getallen = line.split(",");
+        for(String s:getallen){
+            input.add(s.trim());
+        }
+        //print();
+        return input;
+    }
+
+    void print(){
+        for(Object o:input){
+            System.out.println(o);
+        }
+    }
+
+    void calculate(){
+        for(int y=0;y<1;y++){
+            for(int x=0;x<1;x++){}
+        }
+    }
+
+    void log(Object o){
+        System.out.println(o);
+    }
+
+    void log(int i){
+        System.out.println(i);
+    }
+}
